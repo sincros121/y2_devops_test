3ccdc36dc22ed2ac86625f3b3db2349a1e006652
sincros121 <staslevman@gmail.com>
2023-04-03 20:48:09 +0300

    uncommented backend.tf

--- terraform/root_module/backend.tf
+++ terraform/root_module/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "tf-state-test"
-#     region = "us-east-1"
-#     key    = "state/tf.tfstate"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "tf-state-test"
+    region = "us-east-1"
+    key    = "state/tf.tfstate"
+  }
+}
